cb1aba35c167b151afd20f8b9f7283d256df2749
koudenpa <koudenpa@hotmail.com>
2021-02-28 23:27:47 +0900

    ログ出力を試した結果。

--- BlazorWebAssemblySandbox/Program.cs
+++ BlazorWebAssemblySandbox/Program.cs
@@ -17,6 +17,21 @@ namespace BlazorWebAssemblySandbox
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            // Document: https://docs.microsoft.com/ja-jp/aspnet/core/blazor/fundamentals/logging?view=aspnetcore-5.0&pivots=webassembly
+            // ドキュメントからだと分かりづらいが、
+            // 既定で WebAssemblyConsoleLoggerProvider が構成されており ILogger を注入するだけでコンソールにログ出力を行えるようになっている。
+            // https://github.com/dotnet/aspnetcore/blob/d827c653b787c07de908240b7746ce34d3e6271e/src/Components/WebAssembly/WebAssembly/src/Hosting/WebAssemblyHostBuilder.cs#L227-L230
+            builder.Logging
+                // Chromeの開発者ツールの既定では Info レベル以上を表示するようになっている。
+                // Debug 以下のレベルを表示するにはブラウザ側の設定も変更が必要。
+                .SetMinimumLevel(LogLevel.Trace)
+                // 事実上他のプロバイダーは設定しても動かなかったり、設定した瞬間に（ちゃんと）未対応である旨の例外が発生する。
+                // 例えばローカルストレージにログを出力する CustomLoggingProvider を実装する、のようなことはできなくもないようだ。
+                .AddDebug()
+                //.AddConsole()
+                //.AddSimpleConsole()
+                ;
+
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
